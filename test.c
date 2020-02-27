ddd11a0b396bdb7bdf7246643120cda0cae019fe
edunn10 <edunn10@csc208.csudh.edu>
2020-02-26 16:50:51 -0800

    quiz file

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int x = 0;
+	double final = 0;
+	int i;
+	for(i = 0; i < 11; i++)
+	{
+		x = x + 2;
+	}	
+	final = x / 20;
+
+	printf("Value: %.3f \n",final);	
+	
+	return EXIT_SUCCESS;
+}
